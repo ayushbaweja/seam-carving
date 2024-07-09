b1ceb378767cf3ec4ad780f19cf278f65fa19c75
Ayush Baweja <ayushbaweja@gatech.edu>
2024-07-09 04:48:15 -0400

    working sobel filter

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Seam Carving: Content-Aware Image Resizing
 
 ## References
-- [Seam Carving for Content-Aware Image Resizing](https://dl.acm.org/doi/pdf/10.1145/1275808.1276390)
+- [Seam Carving for Content-Aware Image Resizing](https://dl.acm.org/doi/10.1145/1275808.1276390)
 - [Real-world dynamic programming: seam carving](https://avikdas.com/2019/05/14/real-world-dynamic-programming-seam-carving.html)
 - [Seam Carving | Week 2, lecture 7 | 18.S191 MIT Fall 2020](https://www.youtube.com/watch?v=rpB6zQNsbQU)
+- [Content Aware Scaling in C](https://www.youtube.com/watch?v=zrduCu8LYUM&list=PLpM-Dvs8t0VbSJuKZGbseLjICuoQIYt44&index=3)
--- energy-func.py
+++ energy-func.py
@@ -5,6 +5,7 @@ import numpy as np
 
 try:
     im = Image.open("creation_of_adam.jpeg").convert('L') # converts to greyscale
+    im2arr = np.array(im)
 except Exception as e:
     print(e)
 
@@ -29,9 +30,15 @@ Gx = | 2.0   0.0  -2.0 |    and     Gy = |  0.0   0.0   0.0 |
 Gx = np.array([[1.0, 0.0, -1.0], [2.0, 0.0, -2.0], [1.0, 0.0, -1.0]])
 Gy = np.array([[1.0, 2.0, 1.0], [0.0, 0.0, -0.0], [-1.0, -2.0, -1.0]])
 
-sobel_filtered = np.zeros(shape=im.size)
+sobel_filtered = np.zeros_like(im2arr)
 
-for i in range(im.width - 2):
-    for j in range(im.height - 2):
-        # do some matrix multiplcation
-        break
+for i in range(im2arr.shape[0] - 2):
+    for j in range(im2arr.shape[1] - 2):
+        gx = np.sum(np.multiply(Gx, im2arr[i:i+3, j:j+3])) # 3x3 kernel in x direction
+        gy = np.sum(np.multiply(Gy, im2arr[i:i+3, j:j+3])) # 3x3 kernel in y direction
+        sobel_filtered[i+1, j+1] = np.sqrt(gx ** 2 + gy ** 2) # centered at i+1, j+1 pixel
+
+# arr2im = Image.fromarray(im2arr)
+# arr2im.show()
+sobel_Im = Image.fromarray(sobel_filtered)
+sobel_Im.show()
